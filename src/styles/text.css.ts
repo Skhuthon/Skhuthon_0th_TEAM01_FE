f7722da44c304133b0d087f91e46a42a45cc82f7
Kim0426 <706shin1728@naver.com>
2024-06-26 09:33:59 +0900

    chore: setting vanilla-extract receipt

---
+++ src/styles/text.css.ts
@@ -0,0 +1,10 @@
+import { recipe } from '@vanilla-extract/recipes';
+import { vars } from './theme.css';
+
+export const text = recipe({
+  base: {
+    fontSize: vars.size.font_md,
+    color: vars.color.black,
+    fontWeight: vars.weight.medium,
+  },
+});
--- src/styles/theme.css.ts
+++ src/styles/theme.css.ts
@@ -34,4 +34,14 @@ export const [themeClass, vars] = createTheme({
     font_xl: '24px',
     font_xxl: '32px',
   },
+  weight: {
+    thin: '100',
+    extraLight: '200',
+    light: '300',
+    regular: '400',
+    medium: '500',
+    semiBold: '600',
+    bold: '700',
+    extraBold: '800',
+  },
 });
